74431ffaa18b2493692bfc2a5fc71e8ea25dec5d
fgm93 <gonzalo.miguel93@gmail.com>
2015-07-03 23:28:03 -0300

    Singleton de la base

--- src/core/Base.java
+++ src/core/Base.java
@@ -9,21 +9,73 @@ import com.mysql.jdbc.Statement;
 
 public class Base {
 	
-	static String user = "root";
+	
+	public Connection conn;
+    private Statement statement;
+    public static Base db;
+    
+    static String user = "root";
 	static String pass = "";
 	static String host = "jdbc:mysql://localhost:3306/";
 	static String dbName = "grupo_6_db";
-	
-	public static Boolean ExecuteScript(String script)
+    static String driver = "com.mysql.jdbc.Driver";
+    
+    private Base() {
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection)DriverManager.getConnection(host+dbName,user,pass);
+        }
+        catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    public static synchronized Base getDbCon() {
+        if ( db == null ) {
+            db = new Base();
+        }
+        return db;
+ 
+    }
+    
+//	public static Boolean ExecuteScript(String script)
+//	{
+//		Connection conn=null;
+//		Statement st;
+//		
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//			conn = (Connection) DriverManager.getConnection(host + dbName,
+//					user, pass);
+//			st = (Statement) conn.createStatement();
+//
+//			if(st.executeUpdate(script) >0)
+//			{
+//				return true;
+//			}
+//			else{
+//				return false;
+//			}
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return false;
+//		}
+////		finally
+////		{
+////			try {
+////				conn.close();
+////			} catch (SQLException e) {
+////				// TODO Auto-generated catch block
+////				e.printStackTrace();
+////			}
+////		}
+//		
+//	}
+    public static Boolean ExecuteScript(String script)
 	{
-		Connection conn=null;
-		Statement st;
-		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = (Connection) DriverManager.getConnection(host + dbName,
-					user, pass);
-			st = (Statement) conn.createStatement();
+			Statement st = (Statement) db.conn.createStatement();
 
 			if(st.executeUpdate(script) >0)
 			{
@@ -49,18 +101,39 @@ public class Base {
 		
 	}
 	
-	public static ResultSet ExecuteQuery(String script)
+//	public static ResultSet ExecuteQuery(String script)
+//	{
+//		Connection conn=null;
+//		Statement st;
+//		ResultSet rs;
+//		try {			
+//			Class.forName("com.mysql.jdbc.Driver");
+//			conn = (Connection) DriverManager.getConnection(host + dbName,
+//					user, pass);
+//			st = (Statement) conn.createStatement();
+//			rs = st.executeQuery(script);
+//			
+//			return rs;
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			return null;
+//		}
+////		finally
+////		{
+////			try {
+////				conn.close();
+////			} catch (SQLException e) {
+////				// TODO Auto-generated catch block
+////				e.printStackTrace();
+////			}
+////		}
+//	}
+    public static ResultSet ExecuteQuery(String script)
 	{
-		Connection conn=null;
-		Statement st;
-		ResultSet rs;
 		try {			
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = (Connection) DriverManager.getConnection(host + dbName,
-					user, pass);
-			st = (Statement) conn.createStatement();
-			rs = st.executeQuery(script);
-			
+
+			Statement st = (Statement) db.conn.createStatement();
+			ResultSet rs = st.executeQuery(script);
 			return rs;
 		} catch (Exception e) {
 			e.printStackTrace();
